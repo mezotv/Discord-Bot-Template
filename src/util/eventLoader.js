2f3bf1cc13c1de070f5e8f58bf43cdce293df3ea
Mezo <dominik@koch-bautechnik.de>
2022-04-29 22:40:19 +0200

    Small fix

--- src/util/eventLoader.js
+++ src/util/eventLoader.js
@@ -1,12 +1,12 @@
 const { readdir } = require("fs");
 
 module.exports = async (client) => {
-  readdir("./src/event", (err, files) => {
+  readdir("./src/events/", (err, files) => {
     if (err) return console.error(err); 
            
     files.forEach((file) => {
-console.log(files)
-      const event = require(`./src/event/${file}`);
+console.log(file)
+      const event = require(`../src/events/${file}`);
      let eventName = file.split(".")[0];
       client.on(eventName, event.bind(client));
     });
