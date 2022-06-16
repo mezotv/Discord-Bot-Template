6fbd54671999c194b53250949f85df03b6db0cc0
Mezo <dominik@koch-bautechnik.de>
2022-06-16 02:32:59 +0200

    Update interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,17 +1,6 @@
-const { readdirSync } = require("fs");
-
 module.exports = (interaction) => {
 
   const client = interaction.client;
-  const commandFiles = readdirSync("./src/commands/").filter((file) =>
-    file.endsWith(".js")
-  );
-  const commands = [];
-  for (const file of commandFiles) {
-    const command = require(`../commands/${file}`);
-    commands.push(command.data.toJSON());
-    client.commands.set(command.data.name, command);
-  }
   if (!interaction.isCommand()) return;
   const command = client.commands.get(interaction.commandName);
   if (!command) return;
